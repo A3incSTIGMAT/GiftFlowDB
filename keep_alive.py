e8e6898203bf380582093da8f42c025d988300e3
A3incSTIGMAT <brilsash@yandex.ru>
2026-02-19 14:22:28 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "GiftFlow Bot is Alive!"
+    return "🚀 GiftFlow Bot is Alive and Running!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
