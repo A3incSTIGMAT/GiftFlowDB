17debfdc6a8204653b144fd0094054e5335ff93b
A3incSTIGMAT <brilsash@yandex.ru>
2026-03-20 19:15:59 +0800

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,7 @@
+from .start import router as start_router
+from .gifts import router as gifts_router
+from .admin import router as admin_router
+from .donatepay import router as donatepay_router
 
+# Объединяем все роутеры
+routers = [start_router, gifts_router, admin_router, donatepay_router]
