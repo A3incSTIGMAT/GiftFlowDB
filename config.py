1518a7901df075d7f823bd362de15dfdf5f94f76
A3incSTIGMAT <brilsash@yandex.ru>
2026-03-20 19:12:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,24 @@
 import os
 
+# Telegram
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SUPER_ADMIN_ID = int(os.getenv("SUPER_ADMIN_ID", 895844198))
 SUPPORT_ADMIN_ID = int(os.getenv("SUPPORT_ADMIN_ID", 7076299389))
 ADMIN_IDS = [SUPER_ADMIN_ID, SUPPORT_ADMIN_ID]
 
+# DonatePay
 DONATEPAY_API_KEY = os.getenv("DONATEPAY_API_KEY")
 DONATEPAY_WALLET_ID = os.getenv("DONATEPAY_WALLET_ID", "1472367")
 
+# Соцсети
 TWITCH_URL = "https://twitch.tv/lanatwitchh"
 INSTAGRAM_URL = "https://instagram.com/lanawolfyy"
-DONATEPAY_URL = "https://donatepay.ru/don/1472367"
+DONATEPAY_URL = f"https://donatepay.ru/don/{DONATEPAY_WALLET_ID}"
 
+# Данные стримерши
 STREAMER_NAME = "Лана"
 
+# Финансы
 FEE_PERCENT = 0.10
 PROFIT_SPLIT = {
     'lana': 0.47,
@@ -21,3 +26,6 @@ PROFIT_SPLIT = {
     'development': 0.19,
     'tax': 0.06
 }
+
+# База данных (ВАЖНО: сохраняем в /data)
+DB_PATH = os.getenv("DB_PATH", "/data/gift_bot.db")
