93d700f86b86b6550e7d81f8077f90ed278b80c4
A3incSTIGMAT <brilsash@yandex.ru>
2026-03-31 10:03:32 +0800

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,16 @@
+"""
+Инициализация всех обработчиков бота
+"""
+
 from .start import router as start_router
 from .gifts import router as gifts_router
 from .admin import router as admin_router
+from .ozon_payments import router as ozon_router
 
-# Список всех роутеров (без donatepay)
+# Список всех роутеров для подключения в main.py
 routers = [
     start_router,
     gifts_router,
-    admin_router
+    admin_router,
+    ozon_router
 ]
