cfeebb7d61c1f3319fef0eb4a6a03f337484e8fc
A3incSTIGMAT <brilsash@yandex.ru>
2026-03-31 10:32:53 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,22 @@
 import os
 
+# ==================== TELEGRAM ====================
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SUPER_ADMIN_ID = int(os.getenv("SUPER_ADMIN_ID", 895844198))
 SUPPORT_ADMIN_ID = int(os.getenv("SUPPORT_ADMIN_ID", 7076299389))
 ADMIN_IDS = [SUPER_ADMIN_ID, SUPPORT_ADMIN_ID]
 
+# ==================== TELEGRAM КАНАЛ ====================
 CHANNEL_ID = os.getenv("CHANNEL_ID", "@lanatwitchh")
+
+# ==================== СОЦСЕТИ ====================
 TWITCH_URL = "https://twitch.tv/lanatwitchh"
 INSTAGRAM_URL = "https://instagram.com/lanawolfyy"
 
+# ==================== ДАННЫЕ СТРИМЕРШИ ====================
 STREAMER_NAME = "Лана"
 
+# ==================== ФИНАНСЫ ====================
 PROFIT_SPLIT = {
     'lana': 0.47,
     'admin': 0.28,
@@ -18,4 +24,5 @@ PROFIT_SPLIT = {
     'tax': 0.06
 }
 
+# ==================== БАЗА ДАННЫХ ====================
 DB_PATH = os.getenv("DB_PATH", "/app/gift_bot.db")
