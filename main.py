81e4ee9a4d882557d50a23b9dc4f9dadc319fd8a
A3incSTIGMAT <brilsash@yandex.ru>
2026-03-31 09:50:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN, SUPER_ADMIN_ID, SUPPORT_ADMIN_ID
 from database import init_db
 from handlers import routers
+from handlers import ozon_payments
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+# Передаём бота в модуль Озон
+ozon_payments.set_bot(bot)
 
 # Подключаем все роутеры
 for router in routers:
