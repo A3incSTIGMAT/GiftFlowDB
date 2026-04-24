06be5e1f33e30aaefae1c72c124809712a22d289
A3incSTIGMAT <brilsash@yandex.ru>
2026-04-24 18:52:34 +0800

    Add comment for routers list in __init__.py
    Added a comment explaining the routers list.

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,4 +3,5 @@ from .gifts import router as gifts_router
 from .admin import router as admin_router
 from .ozon_payments import router as ozon_router
 
+# Список роутеров для подключения
 routers = [start_router, gifts_router, admin_router, ozon_router]
